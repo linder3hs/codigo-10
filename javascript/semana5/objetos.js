68d4bd21beb4129852395bfa7bd06436cdfc1200
bdiazc90 <bdiazc90@gmail.com>
2022-09-30 20:41:45 -0500

    JS: crearPersonas (objetos literales)

--- javascript/semana5/objetos.js
+++ javascript/semana5/objetos.js
@@ -1,7 +1,7 @@
-let persona;
+const personas = [];
 
 function crearPersona() {
-	persona = {
+	const persona1 = {
 		name: "bruno",
 		years: 32,
 		gender: "masculino",
@@ -22,6 +22,77 @@ function crearPersona() {
 			);
 		},
 	};
+	personas.push(persona1);
+	console.log(personas);
+}
 
-	console.log(persona);
+// Funcion para crear varias 3 personas:
+function crearPersonas() {
+	const persona1 = {
+		name: "bruno",
+		years: 32,
+		gender: "masculino",
+		talla: 179,
+		is_adult: function () {
+			return this.years >= 18;
+		},
+		inches: function () {
+			return this.talla / 2.54;
+		},
+		upper_name: function () {
+			return this.name.toUpperCase();
+		},
+		capital_name: function () {
+			return (
+				this.upper_name().substring(0, 1) +
+				this.name.substring(1, this.name.lenght)
+			);
+		},
+	};
+	const persona2 = {
+		name: "linder",
+		years: 22,
+		gender: "masculino",
+		talla: 170,
+		is_adult: function () {
+			return this.years >= 18;
+		},
+		inches: function () {
+			return this.talla / 2.54;
+		},
+		upper_name: function () {
+			return this.name.toUpperCase();
+		},
+		capital_name: function () {
+			return (
+				this.upper_name().substring(0, 1) +
+				this.name.substring(1, this.name.lenght)
+			);
+		},
+	};
+	const persona3 = {
+		name: "elvia",
+		years: 25,
+		gender: "femenino",
+		talla: 170,
+		is_adult: function () {
+			return this.years >= 18;
+		},
+		inches: function () {
+			return this.talla / 2.54;
+		},
+		upper_name: function () {
+			return this.name.toUpperCase();
+		},
+		capital_name: function () {
+			return (
+				this.upper_name().substring(0, 1) +
+				this.name.substring(1, this.name.lenght)
+			);
+		},
+	};
+	personas.push(persona1);
+	personas.push(persona2);
+	personas.push(persona3);
+	console.log(personas);
 }
