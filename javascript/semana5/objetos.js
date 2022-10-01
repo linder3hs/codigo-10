78ea4bdf378f39a8023e057cddadcf5602fca4a1
bdiazc90 <bdiazc90@gmail.com>
2022-09-30 21:01:35 -0500

    crearPersonas (class objects)

--- javascript/semana5/objetos.js
+++ javascript/semana5/objetos.js
@@ -5,12 +5,12 @@ function crearPersona() {
 		name: "bruno",
 		years: 32,
 		gender: "masculino",
-		talla: 179,
+		height: 179,
 		is_adult: function () {
 			return this.years >= 18;
 		},
 		inches: function () {
-			return this.talla / 2.54;
+			return this.height / 2.54;
 		},
 		upper_name: function () {
 			return this.name.toUpperCase();
@@ -32,12 +32,12 @@ function crearPersonas() {
 		name: "bruno",
 		years: 32,
 		gender: "masculino",
-		talla: 179,
+		height: 179,
 		is_adult: function () {
 			return this.years >= 18;
 		},
 		inches: function () {
-			return this.talla / 2.54;
+			return this.height / 2.54;
 		},
 		upper_name: function () {
 			return this.name.toUpperCase();
@@ -53,12 +53,12 @@ function crearPersonas() {
 		name: "linder",
 		years: 22,
 		gender: "masculino",
-		talla: 170,
+		height: 170,
 		is_adult: function () {
 			return this.years >= 18;
 		},
 		inches: function () {
-			return this.talla / 2.54;
+			return this.height / 2.54;
 		},
 		upper_name: function () {
 			return this.name.toUpperCase();
@@ -74,12 +74,12 @@ function crearPersonas() {
 		name: "elvia",
 		years: 25,
 		gender: "femenino",
-		talla: 170,
+		height: 170,
 		is_adult: function () {
 			return this.years >= 18;
 		},
 		inches: function () {
-			return this.talla / 2.54;
+			return this.height / 2.54;
 		},
 		upper_name: function () {
 			return this.name.toUpperCase();
@@ -94,5 +94,42 @@ function crearPersonas() {
 	personas.push(persona1);
 	personas.push(persona2);
 	personas.push(persona3);
+	console.log(personas);
+}
+
+// Creamos el MOLDE a partir del cual puedo crear multiples personas:
+class Person {
+	constructor(name, years, gender, height) {
+		this.name = name;
+		this.years = years;
+		this.gender = gender;
+		this.height = height;
+	}
+	is_adult() {
+		return this.years >= 18;
+	}
+	inches() {
+		return this.height / 2.54;
+	}
+	upper_name() {
+		return this.name.toUpperCase();
+	}
+	capital_name() {
+		return (
+			this.upper_name().substring(0, 1) +
+			this.name.substring(1, this.name.lenght)
+		);
+	}
+}
+
+// Funcion para crear varias personas a partir de 1 modelo:
+function crearPersonasClass() {
+	const persona1 = new Person("bruno", 32, "masculino", 179);
+	const persona2 = new Person("linder", 22, "masculino", 170);
+	const persona3 = new Person("elvia", 25, "femenino", 170);
+	personas.push(persona1);
+	personas.push(persona2);
+	personas.push(persona3);
+
 	console.log(personas);
 }
