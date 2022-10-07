a30290536aec90edb3734e6030677f5384a2ef5c
linder3hs <linderhassinger00@gmail.com>
2022-10-06 23:33:44 -0500

    chore: movies

--- javascript/semana6/movies/app.js
+++ javascript/semana6/movies/app.js
@@ -1,13 +1,26 @@
 import movies from "./data.json" assert { type: "json" };
 
 const container = document.querySelector(".container");
+const inputBuscar = document.querySelector(".input-buscar");
+const btnBuscar = document.querySelector(".btn-buscar");
 
-function readMovies() {
-  movies.entries.forEach((movie, index) => {
+btnBuscar.onclick = () => {
+  const texto = inputBuscar.value;
+
+  const moviesFiltradas = movies.entries.filter(
+    (movie) => movie.title.toLowerCase() === texto.toLowerCase()
+  );
+
+  readMovies(moviesFiltradas);
+};
+
+function readMovies(listPeliculas = movies.entries) {
+  container.innerHTML = "";
+  listPeliculas.forEach((movie, index) => {
     container.innerHTML += `<div class="movie">
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
-          index + 1
-        }.svg" alt="" />
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${(
+          index + (Math.random() * 10).toFixed(0)
+        ).replace("0", "")}.svg" alt="" />
         <h4>${movie.title}</h4>
         <p>
           ${movie.description}
