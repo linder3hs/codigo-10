8d3cd8df1bb509ae785bd9ae3790dcc2e8216290
linder3hs <linderhassinger00@gmail.com>
2022-10-06 22:31:16 -0500

    chore: movies

--- javascript/semana6/movies/app.js
+++ javascript/semana6/movies/app.js
@@ -1,3 +1,20 @@
 import movies from "./data.json" assert { type: "json" };
 
-console.log(movies);
+const container = document.querySelector(".container");
+
+function readMovies() {
+  movies.entries.forEach((movie, index) => {
+    container.innerHTML += `<div class="movie">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
+          index + 1
+        }.svg" alt="" />
+        <h4>${movie.title}</h4>
+        <p>
+          ${movie.description}
+        </p>
+      </div>
+    `;
+  });
+}
+
+readMovies();
