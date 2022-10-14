33cc60b71160f9074680ce6796184b445f4b7782
linder3hs <linderhassinger00@gmail.com>
2022-10-13 20:48:20 -0500

    edit task

--- javascript/semana7/todolist/index.js
+++ javascript/semana7/todolist/index.js
@@ -7,29 +7,76 @@ let arrayTasks = [];
 
 // !!Nota:Cuando una funcion no tiene nombre se llama funcion anonima
 btnCreate.onclick = function () {
-  const taskText = inputTask.value;
+    const taskText = inputTask.value;
 
-  // Ahora vamos a validar que el taskText no este vacio
-  if (taskText === "") {
-    alert("Debe completar la caja de texto");
-    return;
-  }
+    // Ahora vamos a validar que el taskText no este vacio
+    if (taskText === "") {
+        alert("Debe completar la caja de texto");
+        return;
+    }
 
-  const task = new Task(arrayTasks.length + 1, taskText, new Date(), 1);
-  arrayTasks.push(task);
+    const task = new Task(arrayTasks.length + 1, taskText, new Date(), 1);
+    arrayTasks.push(task);
 
-  listTask.innerHTML += task.render();
+    listTask.innerHTML += task.render();
 
-  inputTask.value = "";
+    inputTask.value = "";
 };
 
 function destroy(id) {
-  // como podemos eliminar un elemento de un array
-  // En la condicion podemos decir que traiga todos los elemenots menos el que tenga
-  // el id que estamos recibiendo
-  //? Que retorna filter? = Array
-  const filterTask = arrayTasks.filter((task) => task._id !== Number(id));
-  // Acá estamos diciendo que el valor. de arrayTask. es ahora filterTask
-  arrayTasks = filterTask;
-  Task.destroyRender(id);
+    // como podemos eliminar un elemento de un array
+    // En la condicion podemos decir que traiga todos los elemenots menos el que tenga
+    // el id que estamos recibiendo
+    //? Que retorna filter? = Array
+    const filterTask = arrayTasks.filter((task) => task._id !== Number(id));
+    // Acá estamos diciendo que el valor. de arrayTask. es ahora filterTask
+    arrayTasks = filterTask;
+    Task.destroyRender(id);
+}
+
+// Edit v1
+/* function edit(id) {
+    const newText = prompt("Ingresa el nuevo nombre de la tarea");
+    // find solo retorna 1 element
+    const oneTask = arrayTasks.find((task) => task._id === id);
+
+    // cuando if ejecuta 1 sola linea de codigo se puede evitar
+    // usar las { }
+    if (oneTask === undefined) return
+
+    oneTask._name = newText;
+
+    Task.updateRender(id, newText);
+} */
+
+// Edit v2
+function edit(id) {
+    const element = document.querySelector(`#task-name-${id}`);
+    element.style.display = "none";
+    // Para poder crear un elemento de type node tenemos que usar la
+    // funcion createElement
+    const container = document.createElement("div"); // <div></div>
+    container.style.display = "flex";
+    container.style.gap = "5px";
+
+    const input = document.createElement("input"); // <input type="text" placeholder="Ingresa el nuevo nombre" />
+    input.type = "text";
+    input.placeholder = "Ingresa el nuevo nombre";
+
+    const button = document.createElement("button"); // <button>Save</button>
+    button.textContent = "Save"
+    button.style.backgroundColor = "#ccc";
+
+    const buttonCancel = document.createElement("button"); // <button>Cancel</button>
+    buttonCancel.textContent = "Cancel"
+    buttonCancel.style.backgroundColor = "#ccc";
+
+    container.appendChild(input); // <div> <input type="text" placeholder="Ingresa el nuevo nombre" /> </div>
+    container.appendChild(button); // <div> <input type="text" placeholder="Ingresa el nuevo nombre" />  <button>Save</button> </div>
+    container.appendChild(buttonCancel); // <div> <input type="text" placeholder="Ingresa el nuevo nombre" />  <button>Save</button> <button>Cancel</button> </div>
+
+    // Recordemos que element es el h6 y queremos poner el container al costado
+    // parentNode.insertBefore = coloca el containe antes del h6
+    // element.nextSibling = cambien la posición a despues del h6
+    element.parentNode.insertBefore(container, element.nextSibling);
 }
--- javascript/semana7/todolist/task.js
+++ javascript/semana7/todolist/task.js
@@ -19,26 +19,34 @@
 //! que nuestra clase sea exportable, es decir que pueda ser
 //! usada desde otro archivo
 class Task {
-  // Estos parametros
-  constructor(id, name, date, status) {
-    this._id = id;
-    this._name = name;
-    this._date = date;
-    this._status = status;
-  }
+    // Estos parametros
+    constructor(id, name, date, status) {
+        this._id = id;
+        this._name = name;
+        this._date = date;
+        this._status = status;
+    }
 
-  static destroyRender(id) {
-    const element = document.querySelector(`#task-${id}`);
-    element.remove();
-  }
+    static destroyRender(id) {
+        const element = document.querySelector(`#task-${id}`);
+        element.remove();
+    }
+
+    static updateRender(id, name) {
+        const texto = document.querySelector(`#task-name-${id}`);
+        // Si quieren cambiar el texto de un elemento pueden usar
+        // * innerText => Antiguo
+        // * textContent => Moderno
+        texto.textContent = name;
+    }
 
   // Es una clase no hace falta usar la palabra function
   render() {
     return `
       <div id="task-${this._id}" class="item__task">
         <input type="checkbox" />
-        <h6>${this._name}</h6>
-        <button>
+        <h6 id="task-name-${this._id}">${this._name}</h6>
+        <button onclick="edit(${this._id})">
           <img src="./images/edit.png" width="15" alt="" />
         </button>
         <button onclick="destroy(${this._id})">
