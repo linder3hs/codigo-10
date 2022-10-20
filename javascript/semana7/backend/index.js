21f4d4a2e299e714f4bd4c0a14a2589334bede3b
linder3hs <linderhassinger00@gmail.com>
2022-10-19 20:47:44 -0500

    chore: bootstrap

--- javascript/semana7/backend/index.js
+++ javascript/semana7/backend/index.js
@@ -1,15 +1,15 @@
 /*
-* Reto #7
-* CONTANDO PALABRAS
-* Dificultad: MEDIA
-*
-* Enunciado: Crea un programa que cuente cuantas veces se repite cada
-*  palabra y que muestre el recuento final de todas ellas.
-* - Los signos de puntuación no forman parte de la palabra.
-* - Una palabra es la misma aunque aparezca en mayúsculas y minúsculas.
-*/
+ * Reto #7
+ * CONTANDO PALABRAS
+ * Dificultad: MEDIA
+ *
+ * Enunciado: Crea un programa que cuente cuantas veces se repite cada
+ *  palabra y que muestre el recuento final de todas ellas.
+ * - Los signos de puntuación no forman parte de la palabra.
+ * - Una palabra es la misma aunque aparezca en mayúsculas y minúsculas.
+ */
 
- /*
+/*
  "Hola, mi nombre es Linder. Mi nombre completo es linder linder (linder3hs)."
 
  Hola = 1
@@ -22,24 +22,31 @@
  */
 
 function countWords(text) {
-    // vamos a crear un arreglo el cual guarde las palabra y su cantidad
-    const result = [];
+  // vamos a crear un arreglo el cual guarde las palabra y su cantidad
+  const result = [];
 
-    text.toLowerCase().split(" ").forEach(function (word) {
-        // Paso 1: Quitar los signos de puntuación
-        const cleanWord = word.replaceAll(/\W/g, "");
-        const search = result.find((res) => res.word === cleanWord);
-        if (search === undefined) {
-            result.push({
-                word: cleanWord,
-                count: 1
-            })
-        } else {
-            search.count++;
-        }
+  text
+    .toLowerCase()
+    .split(" ")
+    .forEach(function (word) {
+      // Paso 1: Quitar los signos de puntuación
+      const cleanWord = word.replaceAll(/\W/g, "");
+      const search = result.find((res) => res.word === cleanWord);
+      if (search === undefined) {
+        result.push({
+          word: cleanWord,
+          count: 1,
+        });
+      } else {
+        search.count++;
+      }
     });
 
-    console.log(result);
+  console.log(result);
 }
 
-countWords("Hola, mi nombre es Linder. Mi nombre completo es linder linder (linder3hs).")
+console.log("dsadsa");
+console.log("2");
+countWords(
+  "Hola, mi nombre es Linder. Mi nombre completo es linder linder (linder3hs)."
+);
