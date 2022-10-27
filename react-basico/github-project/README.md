67b576c2316b52389d0efd426f41cd435b23db31
linder3hs <linderhassinger00@gmail.com>
2022-10-26 22:51:10 -0500

    intro a react

--- react-basico/github-project/README.md
+++ react-basico/github-project/README.md
@@ -53,3 +53,62 @@ function MyCard() {
   );
 }
 ```
+
+## useState
+
+En react para poder almacenar un valor en un variable
+usamos un hook llamando useState
+useState es una funcion de react la cual nos permite poder
+manejar el estado de una variable
+
+- Como se usa useState
+- *Siempre los import de react van en la primera linea*
+
+```js
+import { useState } from "react";
+
+function App() {
+  // useState nos retorna un arreglo con dos valores
+  // el primer valor es el valor de la variable ("Juan")
+  // el segundo valor es una funcion que nos permite cambiar el valor de la variable
+  const [nombre, setNombre] = useState("Juan");
+  
+  // nombre = "Pepe"; // Esto no se puede hacer
+  // setNombre("Pepe"); // Esto si se puede hacer
+
+  // Si queremos cambiar el valor de la variable nombre
+  // tenemos que llamar a la funcion setNombre
+  return (
+    <div>
+      <p>Nombre {nombre}</p>
+      <button onClick={() => setNombre("Pepe")}>Cambiar nombre</button>
+    </div>
+  )
+}
+```
+
+## onChange para Inputs
+
+- En react para poder manejar el valor de un input usams el evento onChange
+este va a permitir captar cada vez que el usuario escriba en el input
+
+```js
+function App() {
+  const [inputText, setInputText] = useState("");
+
+  function handleInputChange(event) {
+    // cambiar el valor de la variable inputText
+    // si queremos obtener lo que el usuario escribio
+    // usamos event.target.value
+    setInputText(event.target.value);
+  }
+
+  return (
+    <div>
+      <p>Nombre {nombre}</p>
+      <input type="text" onChange={handleInputChange} />
+    </div>
+  )
+  
+}
+```
--- react-basico/github-project/src/App.js
+++ react-basico/github-project/src/App.js
@@ -1,9 +1,22 @@
+import { useState } from "react";
 import "./App.css";
-import Header from "./components/Header";
-// Componente: Que es bloque de vista el cual combina HTML y JS
+import hotel from "./assets/icons/hotel.png";
+import link from "./assets/icons/link.png";
+import pin from "./assets/icons/pin.png";
+import profile from "./assets/icons/profile.jpeg";
+import search from "./assets/icons/search.png";
+import sun from "./assets/icons/sun.png";
+import twitter from "./assets/icons/twitter.png";
+
 function App() {
-  function saludar() {
-    console.log("Hola mundo");
+  const [inputText, setInputText] = useState("");
+
+  function handleInputChange(event) {
+    setInputText(event.target.value);
+  }
+
+  function searchUser() {
+    console.log(inputText);
   }
 
   return (
@@ -11,25 +24,29 @@ function App() {
       <div className="section-1">
         <h4 className="title">devfinder</h4>
         <button className="btn-mode">
-          LIGHT <img width="20" src="./images/sun.png" alt="" />
+          LIGHT <img width="20" src={sun} alt="" />
         </button>
       </div>
       <div className="search-container">
         <div className="input-container">
-          <img width="15" src="./images/search.png" alt="" />
+          <img width="15" src={search} alt="" />
           <input
+            value={inputText}
+            onChange={handleInputChange}
             className="input-search"
             type="text"
             placeholder="Search GitHub username..."
           />
         </div>
         <div>
-          <button className="btn-search">Search</button>
+          <button className="btn-search" onClick={searchUser}>
+            Search
+          </button>
         </div>
       </div>
       <div className="information-container">
         <div className="image-container">
-          <img width="100" src="./images/profile.jpeg" alt="" />
+          <img width="100" src={profile} alt="" />
         </div>
         <div className="description-container">
           <div className="user-date-container">
@@ -57,22 +74,22 @@ function App() {
           <div className="info-container">
             <div>
               <p>
-                <img width="15" src="./images/pin.png" alt="" />
+                <img width="15" src={pin} alt="" />
                 &nbsp;San Francisco
               </p>
               <p>
-                <img width="15" src="./images/link.png" alt="" />
+                <img width="15" src={link} alt="" />
                 &nbsp;https://github.blog
               </p>
             </div>
             <div>
               <p>
-                <img width="15" src="./images/twitter.png" alt="" />
+                <img width="15" src={twitter} alt="" />
                 &nbsp;Not avible
               </p>
 
               <p>
-                <img width="15" src="./images/hotel.png" alt="" />
+                <img width="15" src={hotel} alt="" />
                 &nbsp;@linder3hs
               </p>
             </div>
