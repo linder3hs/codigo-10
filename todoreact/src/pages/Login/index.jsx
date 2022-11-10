447e96cf3113d495bbbbad687021c7abcfb9050d
linder3hs <linderhassinger00@gmail.com>
2022-11-09 20:32:55 -0500

    feat: add login validation

--- todoreact/src/pages/Login/index.jsx
+++ todoreact/src/pages/Login/index.jsx
@@ -1,6 +1,32 @@
+import { useState } from "react";
+import Swal from "sweetalert2";
 import "./index.css";
 
 const Login = () => {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleLogin = () => {
+    // null - undefinded - ""
+    if (!user.email || !user.password) {
+      Swal.fire({
+        title: "Error",
+        text: "Completa el correo y password",
+        icon: "error",
+      });
+      return;
+    }
+  };
+
   return (
     <div className="bg__login">
       <div
@@ -21,14 +47,25 @@ const Login = () => {
               type="text"
               className="form-control mt-4"
               placeholder="Ingrese su email"
+              name="email"
+              value={user.email}
+              onChange={handleInputChange}
             />
             <input
               type="password"
               className="form-control mt-4"
               placeholder="Ingrese su password"
+              name="password"
+              value={user.password}
+              onChange={handleInputChange}
             />
             <div className="d-grid">
-              <button className="btn btn-outline-primary mt-4">Ingresar</button>
+              <button
+                className="btn btn-outline-primary mt-4"
+                onClick={handleLogin}
+              >
+                Ingresar
+              </button>
             </div>
           </div>
         </div>
