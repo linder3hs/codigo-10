4733c58c06e4270703a991ef2c54c2db4f9e12e7
linder3hs <linderhassinger00@gmail.com>
2022-11-10 19:40:33 -0500

    feat: add navbar

--- todoreact/src/layouts/MainLayout/index.jsx
+++ todoreact/src/layouts/MainLayout/index.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useNavigate, Outlet } from "react-router-dom";
+import { useNavigate, Outlet, Link } from "react-router-dom";
 
 const MainLayout = () => {
   const history = useNavigate();
@@ -16,7 +16,37 @@ const MainLayout = () => {
 
   return (
     <div>
-      <h1>Main Layout</h1>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo03"
+            aria-controls="navbarTogglerDemo03"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <Link className="navbar-brand" href="/">
+            Todo App
+          </Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link
+                  className="nav-link active"
+                  aria-current="page"
+                  to="/perfil"
+                >
+                  Perfil
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
       <Outlet />
     </div>
   );
